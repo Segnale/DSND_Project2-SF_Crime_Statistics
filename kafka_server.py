379b86f60fde55cb2db8c3f01d6ddd7b9329cf87
Stefano Signor <signor.stefano@gmail.com>
2020-03-22 14:24:34 +0100

    Update kafka_server.py

--- kafka_server.py
+++ kafka_server.py
@@ -1,18 +1,23 @@
 import producer_server
+import logging
+import logging.config
+from pathlib import Path
 
+logging.config.fileConfig(f"{Path(__file__).parents[0]}/logging.ini")
+
+logger = logging.getLogger(__name__)
 
 def run_kafka_server():
 	# DONE get the json file path
     input_file = "police-department-calls-for-service.json"
 
-    # TODO fill in blanks
+    # DONE fill in blanks
     producer = producer_server.ProducerServer(
         input_file=input_file,
         topic="sf.police.calls",
         bootstrap_servers="localhost:9092",
-        client_id=""
+        client_id="None"
     )
-
     return producer
 
 
